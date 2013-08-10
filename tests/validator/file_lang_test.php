8839ddf22056048edd707ac24feea1faefd9951e
Joas Schilling <nickvergessen@gmx.de>
2013-08-10 11:07:07 +0200

    Add unit tests for lang file validation

---
+++ tests/validator/file_lang_test.php
@@ -0,0 +1,35 @@
+<?php
+/**
+*
+* @package phpBB Gallery Testing
+* @copyright (c) 2013 nickvergessen
+* @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+*
+*/
+
+class phpbb_ext_official_translationvalidator_tests_validator_file_lang_test extends phpbb_ext_official_translationvalidator_tests_validator_test_base
+{
+	static public function validate_lang_data()
+	{
+		return array(
+			array('language/lang.php', array(
+				array('fail', 'FILE_INVALID_VARS-language/lang.php-lang'),
+				array('fail', 'LANG_OUTPUT-language/lang.php-' . "\n\n"),
+				array('fail', 'MISSING_KEY-language/lang.php-7_DAYS'),
+				array('warning', 'INVALID_KEY-language/lang.php-8_DAYS'),
+			)),
+			array('language/lang2.php', array(
+				array('fail', 'FILE_INVALID_VARS-language/lang2.php-lang'),
+			)),
+		);
+	}
+
+	/**
+	* @dataProvider validate_lang_data
+	*/
+	public function test_validate_lang($file, $expected)
+	{
+		$this->validator->validate_lang_file($file);
+		$this->assertEquals($expected, $this->message_collection->get_messages());
+	}
+}
