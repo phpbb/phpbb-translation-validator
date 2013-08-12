7bb9ff632f8f7fa517fb0e530b2cf17d9d049a6a
Joas Schilling <nickvergessen@gmx.de>
2013-08-12 23:45:41 +0200

    Try to use CRLF

--- tests/validator/file_line_endings_test.php
+++ tests/validator/file_line_endings_test.php
@@ -22,7 +22,6 @@ class phpbb_ext_official_translationvalidator_tests_validator_file_line_endings_
 	*/
 	public function test_validate_line_endings($file, $expected)
 	{
-		$this->markTestSkipped('Unable to commit CRLF files to the repository');
 		$this->validator->validate_line_endings($file);
 		$this->assertEquals($expected, $this->message_collection->get_messages());
 	}
--- tests/validator/fixtures/tovalidate/line_endings/invalid.php
+++ tests/validator/fixtures/tovalidate/line_endings/invalid.php
@@ -1,2 +1,3 @@
 <?php
+
 $line_endings = 'crlf';
