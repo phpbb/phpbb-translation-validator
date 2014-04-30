1269dc4d70fcb56bc0dd3c472c604c3be8ebd52f
Joas Schilling <nickvergessen@gmx.de>
2014-04-30 15:56:02 +0200

    Add unit test for missing new line in 3.1
    Fix #3

--- tests/validator/file/validate_email_test.php
+++ tests/validator/file/validate_email_test.php
@@ -14,7 +14,7 @@ class validate_email_test extends \official\translationvalidator\tests\validator
 	static public function validate_email_data()
 	{
 		return array(
-			array('email/email.txt', array(
+			array('3.0', 'email/email.txt', array(
 				array('type' => 'fail', 'message' => 'EMAIL_MISSING_SUBJECT-email/email.txt'),
 				array('type' => 'fail', 'message' => 'EMAIL_MISSING_SIG-email/email.txt'),
 				array('type' => 'fail', 'message' => 'EMAIL_ADDITIONAL_VARS-email/email.txt-{TEMPLATE_VAR_DOES_NOT_EXIST}, {U_ACTIVATE*NOT_USING_NORMAL_VAR*}'),
@@ -23,7 +23,16 @@ class validate_email_test extends \official\translationvalidator\tests\validator
 				array('type' => 'fail', 'message' => 'EMAIL_ADDITIONAL_HTML-email/email.txt-&lt;/a&gt;'),
 				array('type' => 'debug', 'message' => 'EMAIL_MISSING_NEWLINE-email/email.txt'),
 			)),
-			array('email/invalid_sig.txt', array(
+			array('3.1', 'email/email.txt', array(
+				array('type' => 'fail', 'message' => 'EMAIL_MISSING_SUBJECT-email/email.txt'),
+				array('type' => 'fail', 'message' => 'EMAIL_MISSING_SIG-email/email.txt'),
+				array('type' => 'fail', 'message' => 'EMAIL_ADDITIONAL_VARS-email/email.txt-{TEMPLATE_VAR_DOES_NOT_EXIST}, {U_ACTIVATE*NOT_USING_NORMAL_VAR*}'),
+				array('type' => 'warning', 'message' => 'EMAIL_MISSING_VARS-email/email.txt-{U_ACTIVATE}'),
+				array('type' => 'fail', 'message' => 'EMAIL_ADDITIONAL_HTML-email/email.txt-&lt;a href=&quot;localhost&quot;&gt;'),
+				array('type' => 'fail', 'message' => 'EMAIL_ADDITIONAL_HTML-email/email.txt-&lt;/a&gt;'),
+				array('type' => 'warning', 'message' => 'EMAIL_MISSING_NEWLINE-email/email.txt'),
+			)),
+			array('3.0', 'email/invalid_sig.txt', array(
 				array('type' => 'fail', 'message' => 'EMAIL_UTF8-email/invalid_sig.txt'),
 				array('type' => 'fail', 'message' => 'EMAIL_INVALID_SIG-email/invalid_sig.txt'),
 				array('type' => 'fail', 'message' => 'EMAIL_ADDITIONAL_VARS-email/invalid_sig.txt-{YEHAA}'),
@@ -34,8 +43,9 @@ class validate_email_test extends \official\translationvalidator\tests\validator
 	/**
 	* @dataProvider validate_email_data
 	*/
-	public function test_validate_email($file, $expected)
+	public function test_validate_email($phpbb_version, $file, $expected)
 	{
+		$this->validator->set_version($phpbb_version);
 		$this->validator->validate_email($file, $file);
 		$errors = $this->message_collection->get_messages();
 
