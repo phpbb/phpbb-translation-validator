29cfbf155dadc124736962853b23f9c5a730528d
Crizzo <mail@crizzo.de>
2017-09-08 16:11:36 +0200

    Use in_array in if-sentence

--- src/Phpbb/TranslationValidator/Validator/FileListValidator.php
+++ src/Phpbb/TranslationValidator/Validator/FileListValidator.php
@@ -120,12 +120,10 @@ class FileListValidator
 		$sourceFiles[] = $this->sourceLanguagePath . 'LICENSE';
 		$sourceFiles = array_unique($sourceFiles);
 
-		/*
-		* Get $lang['direction'] of translation to allow additional rtl-files for rtl-translations
-		*/
+		//Get $lang['direction'] of translation to allow additional rtl-files for rtl-translations
 		include($this->originPath . '/' . $this->originLanguagePath . 'common.php');
 		$direction = $lang['DIRECTION'];
-		// Throw error, if invalid direction is used 
+		// Throw error, if invalid direction is used
 		if (!in_array($direction, array('rtl', 'ltr')))
 		{
 			$this->output->addMessage(Output::FATAL, 'DIRECTION needs to be rtl or ltr');
@@ -206,13 +204,16 @@ class FileListValidator
 					{
 						$level = Output::FATAL;
 					}
-					else if (substr($origin_file, -4 === array('.gif', '.png')) && $direction === 'rtl') {
+					else if (in_array(substr($origin_file, -4), array('.gif', '.png')) && $direction === 'rtl')
+					{
 						$level = Output::WARNING;
 					}
-					else if (substr($origin_file, -3) !==	'.md') {
+					else if (substr($origin_file, -3) !==	'.md')
+					{
 						$level = Output::FATAL;
 					}
-					else  {
+					else
+					{
 						$level = Output::ERROR;
 					}
 
