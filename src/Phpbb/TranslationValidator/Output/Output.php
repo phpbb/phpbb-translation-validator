8c922f37827924c1d97f3ff3d04e67e577bc73ec
battye <battye@phpbb.com>
2019-01-25 04:19:15 +0000

    Update to Symfony 3

--- src/Phpbb/TranslationValidator/Output/Output.php
+++ src/Phpbb/TranslationValidator/Output/Output.php
@@ -213,4 +213,44 @@ class Output implements \Phpbb\TranslationValidator\Output\OutputInterface
 		}
 		return 0;
 	}
+
+	/**
+	 * Returns whether verbosity is quiet (-q).
+	 *
+	 * @return bool true if verbosity is set to VERBOSITY_QUIET, false otherwise
+	 */
+	public function isQuiet()
+	{
+		// TODO: Implement isQuiet() method.
+	}
+
+	/**
+	 * Returns whether verbosity is verbose (-v).
+	 *
+	 * @return bool true if verbosity is set to VERBOSITY_VERBOSE, false otherwise
+	 */
+	public function isVerbose()
+	{
+		// TODO: Implement isVerbose() method.
+	}
+
+	/**
+	 * Returns whether verbosity is very verbose (-vv).
+	 *
+	 * @return bool true if verbosity is set to VERBOSITY_VERY_VERBOSE, false otherwise
+	 */
+	public function isVeryVerbose()
+	{
+		// TODO: Implement isVeryVerbose() method.
+	}
+
+	/**
+	 * Returns whether verbosity is debug (-vvv).
+	 *
+	 * @return bool true if verbosity is set to VERBOSITY_DEBUG, false otherwise
+	 */
+	public function isDebug()
+	{
+		// TODO: Implement isDebug() method.
+	}
 }
